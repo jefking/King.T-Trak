7a35466addd53af82d6455b231103a9b4acafcc1
Jef King <jef@agilebusinesscloud.com>
2014-11-09 20:57:21 -0800

    moved everything

---
+++ King.TTrak/TableStorageReader.cs
@@ -0,0 +1,90 @@
+﻿namespace King.BTrak
+{
+    using King.Azure.Data;
+    using King.BTrak.Models;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Table Storage Reader
+    /// </summary>
+    public class TableStorageReader : ITableStorageReader
+    {
+        #region Members
+        /// <summary>
+        /// Azure Storage Resources
+        /// </summary>
+        protected readonly IAzureStorageResources resources = null;
+
+        /// <summary>
+        /// Table Name
+        /// </summary>
+        protected readonly string tableName = null;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="resources">Azure Storage Resources</param>
+        /// <param name="tableName">Table Name</param>
+        public TableStorageReader(IAzureStorageResources resources, string tableName)
+        {
+            if (null == resources)
+            {
+                throw new ArgumentNullException("resources");
+            }
+            if (string.IsNullOrWhiteSpace(tableName))
+            {
+                throw new ArgumentException("tableName");
+            }
+
+            this.resources = resources;
+            this.tableName = tableName;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Load Tables
+        /// </summary>
+        /// <returns>Tables</returns>
+        public IEnumerable<ITableStorage> Load()
+        {
+            return from t in this.resources.Tables()
+                   where t.Name != this.tableName
+                   select t;
+        }
+
+        /// <summary>
+        /// Retrieve Data
+        /// </summary>
+        /// <param name="tables">Tables</param>
+        /// <returns>SQL Data</returns>
+        public async Task<IEnumerable<TableData>> Retrieve(IEnumerable<ITableStorage> tables)
+        {
+            var datas = new List<TableData>();
+            foreach (var table in tables)
+            {
+                Trace.TraceInformation("Reading from table: {0}.", table.Name);
+
+                var data = new TableData
+                {
+                    TableName = table.Name,
+                    Rows = await table.Query(new TableQuery()),
+                };
+
+                datas.Add(data);
+
+                Trace.TraceInformation("Rows Read: {0}", data.Rows.Count());
+            }
+
+            return datas;
+        }
+        #endregion
+    }
+}
---
+++ King.TTrak/TableStorageWriter.cs
@@ -0,0 +1,83 @@
+﻿namespace King.BTrak
+{
+    using King.Azure.Data;
+    using King.BTrak.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Table Storage Writer
+    /// </summary>
+    public class TableStorageWriter : ITableStorageWriter
+    {
+        #region Members
+        /// <summary>
+        /// Table Storage
+        /// </summary>
+        protected readonly ITableStorage table = null;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="table">Table Storage</param>
+        public TableStorageWriter(ITableStorage table)
+        {
+            if (null == table)
+            {
+                throw new ArgumentNullException("table");
+            }
+
+            this.table = table;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <returns>Success</returns>
+        public virtual async Task<bool> Initialize()
+        {
+            return await this.table.CreateIfNotExists();
+        }
+
+        /// <summary>
+        /// Store Table Data
+        /// </summary>
+        /// <param name="tables">Tables</param>
+        /// <returns>Task</returns>
+        public virtual async Task Store(IEnumerable<TableSqlData> tables)
+        {
+            foreach (var tableData in tables)
+            {
+                var hasPk = null != tableData.PrimaryKeyColumns && tableData.PrimaryKeyColumns.Any();
+                foreach (var entity in tableData.Rows)
+                {
+                    entity.Add(TableStorage.PartitionKey, tableData.TableName);
+
+                    var rowKey = string.Empty;
+                    if (hasPk)
+                    {
+                        foreach (var col in tableData.PrimaryKeyColumns)
+                        {
+                            rowKey += string.IsNullOrWhiteSpace(rowKey) ? string.Empty : "_";
+                            rowKey += null == entity[col] ? "(null)" : entity[col].ToString();
+                        }
+                    }
+                    else
+                    {
+                        rowKey = Guid.NewGuid().ToString();
+                    }
+
+                    entity.Add(TableStorage.RowKey, rowKey);
+                    await this.table.InsertOrReplace(entity);
+                }
+            }
+        }
+        #endregion
+    }
+}
