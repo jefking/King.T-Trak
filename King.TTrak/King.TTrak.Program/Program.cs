04ede412b6bdce342a390c6cb1e00428023d209a
Jef King <jef@agilebusinesscloud.com>
2014-11-09 20:36:06 -0800

    Icons

--- King.TTrak/King.TTrak.Program/Program.cs
+++ King.TTrak/King.TTrak.Program/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace King.TTrak.Program
+﻿namespace King.TTrak.Program
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
