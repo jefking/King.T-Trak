1df4cc2b033698515c5d9d53f896aee917e1fac5
Jef King <jef@agilebusinesscloud.com>
2014-11-09 21:00:26 -0800

    Configuration

--- King.TTrak/Models/ConfigValues.cs
+++ King.TTrak/Models/ConfigValues.cs
@@ -1,6 +1,46 @@
 ﻿namespace King.TTrak.Models
 {
+    /// <summary>
+    /// Configuration Values
+    /// </summary>
     public class ConfigValues : IConfigValues
     {
+        #region Properties
+        /// <summary>
+        /// From Table Name
+        /// </summary>
+        public string FromTable
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// To Table Name
+        /// </summary>
+        public string ToTable
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// From Connection String
+        /// </summary>
+        public string FromConnectionString
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// To Connection String
+        /// </summary>
+        public string ToConnectionString
+        {
+            get;
+            set;
+        }
+        #endregion
     }
 }
--- King.TTrak/Models/Interfaces.cs
+++ King.TTrak/Models/Interfaces.cs
@@ -7,6 +7,37 @@
     public interface IConfigValues
     {
         #region Properties
+        /// <summary>
+        /// From Table Name
+        /// </summary>
+        string FromTable
+        {
+            get;
+        }
+
+        /// <summary>
+        /// To Table Name
+        /// </summary>
+        string ToTable
+        {
+            get;
+        }
+
+        /// <summary>
+        /// From Connection String
+        /// </summary>
+        string FromConnectionString
+        {
+            get;
+        }
+
+        /// <summary>
+        /// To Connection String
+        /// </summary>
+        string ToConnectionString
+        {
+            get;
+        }
         #endregion
     }
     #endregion
